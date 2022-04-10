13eb797dfddb60227a2b4fc9ba89e68ad3e6ae60
zhoujm <1820732570@qq.com>
2022-04-10 23:41:45 +0800

    更新Search页面

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,11 @@
 import requests from "./axios";
 import mockRequests from './mockAxios'
 
+//三级联动分类请求
 export const reqCategoryList = () => requests({ url: '/product/getBaseCategoryList', method: 'get' })
-
+//轮播图数据请求
 export const reqBannerList = () => mockRequests.get('/banner')
-
+//楼层信息数据请求
 export const reqFloorList = () => mockRequests.get('/floor')
+//搜索模块数据请求
+export const reqSearchInfo = (params) => requests({ url: '/list', method: 'post', data: params })
--- src/store/search/index.js
+++ src/store/search/index.js
@@ -1,11 +1,33 @@
-const state = {}
-const mutations = {}
-const getters = {}
-const action = {}
+import { reqSearchInfo } from '@/api'
+
+const state = {
+    searchList: {},
+}
+const mutations = {
+    updateSearchList(state,searchList) {
+        state.searchList = searchList
+    }
+}
+
+const actions = {
+    //请求搜索页面数据
+    async getSearchList({commit},params = {}) {
+        const result = await reqSearchInfo(params)
+        if(result.code === 200) {
+            commit('updateSearchList',result.data)
+        }
+    }
+}
+
+const getters = {
+    goodsList(state) {
+        
+    }
+}
 
 export default {
     state,
     mutations,
     getters,
-    action
+    actions
 }
